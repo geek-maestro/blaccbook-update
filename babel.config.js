df80ae00ffdcdcf4058526836de4374320cd469a
geek-maestro <josepheshun819@gmail.com>
2025-04-05 21:13:32 +0000

    initial setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
+  const plugins = ['react-native-paper/babel'];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
